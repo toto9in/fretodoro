177d5fa6a2b5f5b1b9d8aa77c24a215f57f3154e
toto9 <aaba03@gmail.com>
2026-03-12 08:27:06 -0300

    feat: add window-state plugin and increase default window size to 1280x800

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -97,6 +97,7 @@ fn get_today_routine(app: AppHandle) -> Result<Option<DayRoutine>, String> {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_window_state::Builder::new().build())
         .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_prevent_default::init())
         .plugin(tauri_plugin_opener::init())
